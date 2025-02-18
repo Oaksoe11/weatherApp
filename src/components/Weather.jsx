d148ee9077af1c86b60085c0c7384d2a92fe2c0d
Oaksoe11 <osk@sfu.ca>
2025-02-17 23:35:30 -0800

    Implement search bar autoFocus

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -6,7 +6,7 @@ const Weather = () => {
   return (
     <div className='weather'>
         <div className='search-bar'>
-            <input type="text" placeholder='Search' />
+            <input type="text" placeholder='Search' autoFocus />
             <img src={search_icon} alt="" />
 
         </div>
