a44bdf0f22522eecd072583b73bc102638116e91
Oaksoe11 <osk@sfu.ca>
2025-02-18 00:56:41 -0800

    Implement display data from API

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -52,7 +52,7 @@ const Weather = () => {
     }
 
     useEffect(()=>{
-        search("Mawlamyine");
+        search("Yangon");
     },[])
 
 
@@ -63,15 +63,15 @@ const Weather = () => {
             <img src={search_icon} alt="" />
 
         </div>
-        <img src={clear_icon} alt="" className='weather-icon' />
-        <p className='temperature'>16*C</p>
-        <p className='location'>London</p>
-        <p className='location_country'>UK</p>
+        <img src={weatherData.icon} alt="" className='weather-icon' />
+        <p className='temperature'>{weatherData.temperature}*C</p>
+        <p className='location'>{weatherData.location}</p>
+        <p className='location_country'>{weatherData.country}</p>
         <div className='weather-data'>
             <div className='col'>
                 <img src={humidity_icon} alt="" />
                 <div>
-                    <p>91 %</p>
+                    <p>{weatherData.humidity} %</p>
                     <span>Humidity</span>
                 </div>
             </div>
@@ -79,7 +79,7 @@ const Weather = () => {
             <div className='col'>
                 <img src={wind_icon} alt="" />
                 <div>
-                    <p>3.6 Km/h</p>
+                    <p>{weatherData.windSpeed}</p>
                     <span>Wind Speed</span>
                 </div>
             </div>
